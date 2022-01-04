2e3e2912361f4cdc39c1e2764eff98c327ee48be
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2022-01-04 14:03:40 -0500

    added tutorial schema:

--- server/models/Tutorial.js
+++ server/models/Tutorial.js
@@ -1,7 +1,10 @@
 const { Schema, model } = require('mongoose');
 
 const tutorialSchema = new Schama({
-    // TODO
+    steps: [String],
+    name: String,
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    category: { type: Schema.Types.ObjectId, ref: 'Category' },
 });
 
 const Tutorial = model('Tutorial', tutorialSchema);
