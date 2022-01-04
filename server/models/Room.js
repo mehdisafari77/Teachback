077f3df44806b13add65801f19c58c93c7033a9b
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2022-01-04 14:06:23 -0500

    added Room schema

--- server/models/Room.js
+++ server/models/Room.js
@@ -1,7 +1,10 @@
 const { Schema, model } = require('mongoose');
 
 const roomSchema = new Schama({
-    // TODO
+    connected: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    owner: { type: Schema.Types.ObjectId, ref: 'User' },
+    currentStep: Number,
+    tutorial: { type: Schema.Types.ObjectId, ref: 'Tutorial' },
 });
 
 const Room = model('Room', roomSchema);
