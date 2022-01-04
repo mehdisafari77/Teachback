cd6dec149b4d44c0437d81bb8a2b5387e32410ea
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2022-01-04 14:39:06 -0500

    added tutorials resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,40 @@
+const { Tutorial, User, Room, Category } = require('../models');
+
 const resolvers = {
     Query: {
-        // TODO
+        Tutorials: async (name, categoryID) => {
+            return await Tutorial.find({
+                name: new RegExp(name, 'i'),
+                category: categoryID
+            }).populate('category').populate('author');
+        },
+        Room: async (roomID) => {
+            // TODO
+            return null;
+        },
+        Categories: () => {
+            // TODO
+            return null;
+        }
     },
 
     Mutation: {
-        // TODO
+        CreateTutorial: (steps, name, categoryID) => {
+            // TODO
+            return null;
+        },
+        CreateRoom: (tutorialID, ownerID) => {
+            // TODO
+            return null;
+        },
+        CreateUser: (email, password, username) => {
+            // TODO
+            return null;
+        },
+        Login: (username, password) => {
+            // TODO
+            return null;
+        }
     }
 }
 
