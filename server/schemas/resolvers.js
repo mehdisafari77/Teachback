8ca6c48090927c826418e21d7a9bb10608d5b8a0
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2022-01-13 15:45:40 -0800

    tweaked README and styling

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,7 +8,8 @@ const resolvers = {
             let options = {};
             // TODO - scrub text of regular expressions. Escape spacial characters.
             if(name) options.name = new RegExp(name, "i");
-            if(categoryID) options.category = categoryID
+            if(categoryID) options.category = categoryID;
+            
             return await Tutorial.find(options).populate('category').populate('author');
         },
         Room: async (roomID) => {
@@ -16,13 +17,12 @@ const resolvers = {
             return null;
         },
         Categories: async () => {
-            // TODO
             return await Category.find();
         }
     },
 
     Mutation: {
-        CreateTutorial: (steps, name, categoryID) => {
+        CreateTutorial: (steps, name, categoryID, userID) => {
             // TODO
             return null;
         },
